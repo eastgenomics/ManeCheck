c579e0157a7f54bc033bdea263375a957e36ef0e
Chris Pyatt <chris-pyatt@live.co.uk>
2022-08-02 17:26:31 +0100

    robust indexing and printout of variants with neither MANE transcript

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # ManeCheck
 
 Script checks a VEP annotated VCF file (product of running a vcf through VEP with the MANE option selected) to count the number of variants from that VCF that appear in a MANE transcript.
+
+Add the verbose flag to get a printout of the results dictionary (one entry per vcf record)
+
+
+
+Example output:
+
+###############################################################
+Total records: 90, No. MANE Select: 88, No. MANE Clinical: 86
+###############################################################
+Variants below are not associated with any MANE transcript:
+Record(CHROM=7, POS=140739814, REF=G, ALT=[C])
+###############################################################
--- mane_check.py
+++ mane_check.py
@@ -17,48 +17,66 @@ parser.add_argument(
     )
 args = parser.parse_args()
 
+with open(args.vcf, 'r') as infile:
+    vcf_reader = vcf.Reader(infile)
 
-vcf_reader = vcf.Reader(open(args.vcf, 'r'))
+    record_count = 0
+    mane_select_count = 0
+    mane_clinical_count = 0
+    results = {}
+    lacking_both = []
 
-record_count = 0
-mane_select_count = 0
-mane_clinical_count = 0
-results = {}
+    info_str = vcf_reader.infos
+    format_str = info_str['CSQ'].desc.split('Format: ')[1]
+    format_list = format_str.split('|')
 
-for record in vcf_reader:
-    record_count = record_count + 1
-    info = record.INFO
-    ensembl_id = []
-    mane_select = []
-    mane_clinical = []
-    # get all the transcript IDs from the list of consequences
-    for transcript in info['CSQ']:
-        if transcript.split('|')[23] != '':
-            ensembl_id.append(transcript.split('|')[6])
-            mane_select.append(transcript.split('|')[23])
-        if transcript.split('|')[24] != '':
-            ensembl_id.append(transcript.split('|')[6])
-            mane_clinical.append(transcript.split('|')[24])
-    
-    unique_mane_select = set(mane_select)
-    unique_mane_clinical = set(mane_clinical)
+    csq_dict = {}
+    for i, j in enumerate(format_list):
+        csq_dict[j] = i
 
-    if unique_mane_select:
-        mane_select_count = mane_select_count + 1
-    if unique_mane_clinical:
-        mane_clinical_count = mane_clinical_count + 1
+    for record in vcf_reader:
+        record_count = record_count + 1
+        info = record.INFO
+        ensembl_id = []
+        mane_select = []
+        mane_clinical = []
+        # get all the transcript IDs from the list of consequences
+        for transcript in info['CSQ']:
+            if transcript.split('|')[csq_dict['MANE_SELECT']] != '':
+                ensembl_id.append(transcript.split('|')[csq_dict['Feature']])
+                mane_select.append(transcript.split('|')[csq_dict['MANE_SELECT']])
+            if transcript.split('|')[csq_dict['MANE_PLUS_CLINICAL']] != '':
+                ensembl_id.append(transcript.split('|')[csq_dict['Feature']])
+                mane_clinical.append(transcript.split('|')[csq_dict['MANE_PLUS_CLINICAL']])
+        
+        unique_mane_select = set(mane_select)
+        unique_mane_clinical = set(mane_clinical)
 
-    results[str(record)] = {'ensembl_ids': set(ensembl_id),
-                            'mane_select_ids': unique_mane_select,
-                            'mane_clinical_ids': unique_mane_clinical}
+        if not unique_mane_select and not unique_mane_clinical:
+            lacking_both.append(str(record))
+
+        if unique_mane_select:
+            mane_select_count = mane_select_count + 1
+        if unique_mane_clinical:
+            mane_clinical_count = mane_clinical_count + 1
+
+        results[str(record)] = {'ensembl_ids': set(ensembl_id),
+                                'mane_select_ids': unique_mane_select,
+                                'mane_clinical_ids': unique_mane_clinical}
 
 sep = '###############################################################'
 
 print(
     f'{sep}\nTotal records: {record_count}, \
 No. MANE Select: {mane_select_count}, \
-No. MANE Clinical: {mane_clinical_count}\n{sep}'
+No. MANE Clinical: {mane_clinical_count}\n{sep}\n\
+Variants below are not associated with any MANE transcript:'
 )
+
+for i in lacking_both:
+    print(i)
+
+print(sep)
 
 if args.verbose:
     for key, value in results.items():
